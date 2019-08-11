4bb27ecb03c13e5be7be015e73e0695d7ff6c6ed
Cedric Hanebaum <Cedric.Hanebaum@tsaro.io>
2019-08-11 19:20:23 +0200

    Added some classes for integer linear algebra, because minecraft's native ones just didn't cut it.

---
+++ src/main/java/com/alcatrazescapee/oreveins/util/Plane3i.java
@@ -0,0 +1,43 @@
+package com.alcatrazescapee.oreveins.util;
+
+import net.minecraft.util.math.BlockPos;
+
+public class Plane3i
+{
+    private Vec3i p;
+    private Vec3i n;
+
+    public Plane3i(Vec3i pPoint, Vec3i pNormal)
+    {
+        this.p = pPoint;
+        this.n = pNormal;
+    }
+
+    public boolean isOnPlane(Vec3i pPoint)
+    {
+        Vec3i p0 = p.subtract(pPoint);
+        return p0.dot(n) == 0;
+    }
+
+    public boolean isOnPlane(BlockPos pPos)
+    {
+        return isOnPlane(new Vec3i(pPos.getX(), pPos.getY(), pPos.getZ()));
+    }
+
+    public Vec3i getIntersectionWithLine(Vec3i pPoint, Vec3i pNormal)
+    {
+        if(pNormal.dot(n) == 0) return new Vec3i(0, Integer.MIN_VALUE, 0);
+        int d = n.dot(p.subtract(pPoint)) / pNormal.dot(n);
+        return pPoint.add(pNormal.multiply(d));
+    }
+
+    public Vec3i getNormal()
+    {
+        return n;
+    }
+
+    public Vec3i getPoint()
+    {
+        return p;
+    }
+}
---
+++ src/main/java/com/alcatrazescapee/oreveins/util/Vec3i.java
@@ -0,0 +1,75 @@
+package com.alcatrazescapee.oreveins.util;
+
+import net.minecraft.util.math.BlockPos;
+
+public class Vec3i
+{
+    private int x;
+    private int y;
+    private int z;
+
+    public Vec3i(int pX, int pY, int pZ)
+    {
+        this.x = pX;
+        this.y = pY;
+        this.z = pZ;
+    }
+
+    public Vec3i(BlockPos pos)
+    {
+        this.x = pos.getX();
+        this.y = pos.getY();
+        this.z = pos.getZ();
+    }
+
+    public Vec3i add(Vec3i pV)
+    {
+        return new Vec3i(x + pV.x, y + pV.y, z + pV.z);
+    }
+
+    public Vec3i multiply(int pScalar)
+    {
+        return new Vec3i(pScalar * x, pScalar * y, pScalar * z);
+    }
+
+    public Vec3i subtract(Vec3i pV)
+    {
+        return this.add(pV.multiply(-1));
+    }
+
+    public int dot(Vec3i pV)
+    {
+        return x * pV.x + y * pV.y + z * pV.z;
+    }
+
+    public Vec3i cross(Vec3i pV)
+    {
+        return new Vec3i(y * pV.z - z * pV.y, z * pV.x - x * pV.z, x * pV.y - y * pV.x);
+    }
+
+    public int getLength()
+    {
+        return (int)Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public int getZ()
+    {
+        return z;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "(" + x + "," + y + "," + z + "," + ")";
+    }
+}
